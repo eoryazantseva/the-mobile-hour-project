5cb967bf36126d5bceb965dfeff2370b37f9f8ad
eoryazantseva <eoryazantseva@gmail.com>
2022-09-02 00:26:41 +1000

    changed  sign up form php file

--- connect.php
+++ connect.php
@@ -1,22 +1,38 @@
 <?php
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$cust_email = $_POST['cust_email'];
-$password = $_POST['password'];
 
+$servername = "localhost";
+$database = "u969596019_themobilehour";
+$username = "u969596019_ryazantseva";
+$password = "Osmandina!123";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
 
-<!-- Database connection -->
-
-$conn = new mysqli('localhost','root','','themobilehour');
-if($conn->connect_error){
-    die('Connection Failed : '.$conn->connect_error);
-}else{
-    $stmt = $conn->prepare("insert into registration(firstName, lastName, cust_email, password)
-    values(?, ?, ?, ?)");
-    $stmt->bind_param("ssss",$firstName, $lastName, $cust_email, $password);
-    $stmt->execute();
-    echo "Thank you for the registration";
-    $stmt->close();
-    $conn->close();
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+echo "Connected successfully";
+
+
+// Taking al 4 values from the form data(input)
+
+$firstName = $_REQUEST['firstName'];
+$lastName = $_REQUEST['lastName'];
+$cust_email = $_REQUEST['cust_email'];
+$password = $_REQUEST['password'];
+
+// Performing insert query execution
+
+$sql = "INSERT INTO customer VALUES ('$firstName','$lastName','$cust_email','$password')";
+
+if(mysqli_query($conn, $sql)){
+    echo "<h3> data stored in a database successfully <h3>"
+} else{
+    echo "ERROR: Hush! Sorry $sql. "
+    . mysqli_error($conn);
+}
+
+// Close connection
+
+mysqli_close($conn);
 ?>
