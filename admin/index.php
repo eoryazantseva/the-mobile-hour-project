b48977ce0e002c452c5cf19ae676526f4f59adf4
eoryazantseva <eoryazantseva@gmail.com>
2022-09-16 00:33:40 +1000

    show values from db on admin product page

--- admin/index.php
+++ admin/index.php
@@ -9,101 +9,62 @@ if (!isset($_SESSION['username'])) {
 
 
 
-  <div class="row">
-    <?php include "./templates/sidebar.php"; ?>
-    <div class="col-md-10 bg-white main">
+  <div class="row mt-5">
+        <?php include "./templates/sidebar.php"; ?>
+        <div class="col-md-10 bg-white main mt-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-10">
+                        <h2>Product List</h2>
+                    </div>
+                    <div class="col-2">
+                        <a href="#" data-toggle="modal" data-target="#add_product_modal" class="btn btn-primary btn-sm">Add Product</a>
+                    </div>
+                </div>
 
-      <div class="row">
-      	  <div class="col-10">
-      		  <h2>Product List</h2>
-      	  </div>
-      	  <div class="col-2">
-      		  <a href="#" data-toggle="modal" data-target="#add_product_modal" class="btn btn-primary btn-sm">Add Product</a>
-      	  </div>
+
+                <?php
+                $host    = 'localhost';
+                $user    = 'u969596019_ryazantseva';
+                $pass    = 'Osmandina!123';
+                $db_name = 'u969596019_themobilehour';
+
+                //create connection
+                mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+                $connection = mysqli_connect($host, $user, $pass, $db_name);
+
+                //get results from database
+                $result = mysqli_query($connection, "SELECT * FROM product");
+                $all_property = array();  //declare an array for saving property
+
+                //showing property
+                echo '<table class="table">
+                    <thead>
+                        <tr>';  //initialize table tag
+                        while ($property = mysqli_fetch_field($result)) {
+                            echo '<th scope="col">' . $property->name . '</th>';  //get field name for header
+                            $all_property[] = $property->name;  //save those to array
+                            }
+                        echo '</tr>
+                    /thead>'; //end tr tag
+
+
+                    echo '<tbody>';
+                    //showing all data
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "<tr>";
+                            foreach ($all_property as $item) {
+                                echo '<td>' . $row[$item] . '</td>'; //get items using property value
+                            }
+                            echo '</tr>
+                    </tbody>';
+                                    }
+                echo "</table>";
+
+                mysqli_close($connection);
+                ?>
+            </div>
+
         </div>
-
-<?php
-
-    $con=mysqli_connect('localhost', 'u969596019_ryazantseva', 'Osmandina!123', 'u969596019_themobilehour');
-    // Check connection
-    if (mysqli_connect_errno())
-    {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-
-    $result = mysqli_query($con,"SELECT * FROM product");
-
-    echo "<table class='table table-striped table-sm' border='1'>
-        <tr>
-            <th>Name</th>
-            <th>Model</th>
-            <th>Manufacturer</th>
-            <th>Price</th>
-            <th>Stock</th>
-            <th>Feature</th>
-            <th>Action</th>
-        </tr>";
-
-        while($row = mysqli_fetch_array($result))
-        {
-        echo "<tr>";
-        echo "<td>" . $row['Name'] . "</td>";
-        echo "<td>" . $row['Model'] . "</td>";
-        echo "<td>" . $row['Manufacturer'] . "</td>";
-        echo "<td>" . $row['Price'] . "</td>";
-        echo "<td>" . $row['Stock'] . "</td>";
-        echo "<td>" . $row['Feature'] . "</td>";
-        echo "<td>" . $row['Action'] . "</td>";
-        echo "</tr>";
-        }
-      echo "</table>";
-
-mysqli_close($con);
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <!-- <div class="table-responsive">
-            <table class="table table-striped table-sm">
-            <thead>
-                <tr>
-                    <th> <i class="fa-regular fa-image"></i></th>
-                    <th>Name</th>
-                    <th>Model</th>
-                    <th>Manufacturer</th>
-                    <th>Price</th>
-                    <th>Stock</th>
-                    <th>Feature</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody id="product_list">
-                <tr>
-                    <td><img src="../images/iPhoneX.jpg" alt=""></td>
-                    <td>Huawei Y6s (Dual Sim 4G, 6.09", 64GB/3GB) - Orchid Blue</td>
-                    <td>JAT-LX3</td>
-                    <td>Huawei</td>
-                    <td>$329</td>
-                    <td>$20</td>
-                    <th>Feature1</th>
-                    <th><a class="btn btn-sm btn-info"></a><a class="btn btn-sm btn-danger">Delete</a></th>
-                </tr>
-            </tbody>
-            </table>
-        </div>
-        <a href="new-product.php" role="button" class="btn btn-secondary"> Create New </a> -->
-
-    </div>
-
     </div>
 
